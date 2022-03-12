1d7e60cdcc90ec27a53b7800aae59f098af57d22
Khurshida-Meem <meemrumi59@gmail.com>
2022-03-13 02:38:36 +0600

    updated goals margin padding

--- src/Pages/Home/Goals/Goals.js
+++ src/Pages/Home/Goals/Goals.js
@@ -3,15 +3,17 @@ import './Goals.css'
 
 const Goals = () => {
     return (
-        <div className="-mt-16 container relative  md:flex justify-evenly">
-            <div className='font-bold text-2xl px-20 py-14 bg-green mt-2'>
-                Priority
-            </div>
-            <div className='font-bold text-2xl px-20 py-14 bg-salmon mt-2'>
-                Security
-            </div>
-            <div className='font-bold text-2xl px-20 py-14 bg-sand mt-2'>
-                Comfort
+        <div className='flex justify-center mx-3'>
+            <div className="-mt-16 container relative  md:flex justify-evenly">
+                <div className='font-bold text-2xl text-center px-20 py-14 bg-green mt-2'>
+                    Priority
+                </div>
+                <div className='font-bold text-2xl text-center px-20 py-14 bg-salmon mt-2'>
+                    Security
+                </div>
+                <div className='font-bold text-2xl text-center px-20 py-14 bg-sand mt-2'>
+                    Comfort
+                </div>
             </div>
         </div>
     );
