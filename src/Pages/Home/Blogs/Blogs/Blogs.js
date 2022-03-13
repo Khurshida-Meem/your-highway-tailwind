09ef18de1d4c222bdcda45581890d53506470c68
Khurshida-Meem <meemrumi59@gmail.com>
2022-03-13 14:06:02 +0600

    added loading spinner

--- src/Pages/Home/Blogs/Blogs/Blogs.js
+++ src/Pages/Home/Blogs/Blogs/Blogs.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { Triangle } from 'react-loader-spinner'
 import Blog from './Blog'
 
 const Blogs = () => {
@@ -23,7 +22,7 @@ const Blogs = () => {
 
     return (
         <div className='container mt-20'>
-            {approvedBlogs.lengtn ? <div>
+            <div>
                 <h1 className='text-center text-3xl mb-16'> <span className='component-header px-10 py-2'>Featured Blogs</span></h1>
                 <div>
                     {approvedBlogs.map((blog) => <Blog
@@ -31,12 +30,7 @@ const Blogs = () => {
                         blog={blog}
                     ></Blog>)}
                 </div>
-            </div> :
-                <div className='container flex justify-center mt-10' >
-                    <Triangle color="#ef4565" height={100} width={200}
-                    />
-                </div>
-            }
+            </div>
 
         </div>
 
